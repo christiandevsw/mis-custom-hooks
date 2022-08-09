f9f4dcc232dc6c363e1a981332bdd1982636a3fc
Christian Ramirez <christian.devsw@gmail.com>
2022-08-09 09:09:06 -0500

    Un commit

---
+++ useTodos.js
@@ -0,0 +1,58 @@
+import { useEffect, useReducer } from 'react';
+import { todoReducer } from '../08-useReducer';
+
+const initialState = [
+  // {
+  //   id: new Date().getTime(),
+  //   description: 'la piedra del alba',
+  //   done: false,
+  // },
+  // {
+  //   id: new Date().getTime() * 2,
+  //   description: 'la piedra del anochecer',
+  //   done: false,
+  // },
+];
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || [];
+};
+
+export const useTodos = () => {
+  const [todos, dispatch] = useReducer(todoReducer, initialState, init);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos));
+  }, [todos]);
+
+  const handleNewTodo = todo => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    };
+    dispatch(action);
+  };
+
+  const handleDeleteTodo = id => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    });
+  };
+
+  const handleToggleTodo = id => {
+    dispatch({
+      type: '[TODO] Toogle Todo',
+      payload: id,
+    });
+  };
+
+  return {
+    todos,
+    todosCount: todos.length,
+    pendingTodosCount: todos.filter(todo => !todo.done).length,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  };
+};
